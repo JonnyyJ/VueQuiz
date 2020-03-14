a6d91b992c918c632d3133a3ee3241bde07ac486
Jonnyy <caijishiwo@gmail.com>
2020-03-13 23:32:26 -0400

    import bootstrap vue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import HelloWorld from './components/HelloWorld.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 
